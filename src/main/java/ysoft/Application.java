d75556a20b12123e3e92e460e117bbd755136c61
dsturm <dsturm@casenetllc.com>
2016-10-06 15:30:22 +0200

    obsolete annotation removed

--- src/main/java/ysoft/Application.java
+++ src/main/java/ysoft/Application.java
@@ -2,14 +2,12 @@ package ysoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by sturm on 30.08.2016.
  *
  */
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
